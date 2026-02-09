3637baa04ad9753ee9879a5dc57fda5ff297ef8e
Pretham G <unknownhawk804@gmail.com>
2026-02-09 11:38:56 +0530

    sample commit

--- src/main/java/com/project/JobApp/JobAppApplication.java
+++ src/main/java/com/project/JobApp/JobAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobAppApplication {
-
+    //mic test
 	public static void main(String[] args) {
 		SpringApplication.run(JobAppApplication.class, args);
 	}
